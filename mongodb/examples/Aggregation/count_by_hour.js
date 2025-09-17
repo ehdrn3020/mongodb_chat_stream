c0127b8b1a7cb201f39b0e0f49ea36ddac384f8d
ehdrn3020 <ehdrn3020@gmail.com>
2025-09-17 22:49:34 +0900

    집계 테스트 1

---
+++ mongodb/examples/Aggregation/count_by_hour.js
@@ -0,0 +1,9 @@
+// 방별, 시간대별 메시지 건수
+db.chat_messages.aggregate([
+  {$project:{
+    roomId:1,
+    hour: {$dateTrunc:{date:"$ts", unit:"hour"}}
+  }},
+  {$group:{_id:{room:"$roomId", hour:"$hour"}, cnt:{$sum:1}}},
+  {$sort: {"_id.room":1, "_id.hour":1}}
+])
