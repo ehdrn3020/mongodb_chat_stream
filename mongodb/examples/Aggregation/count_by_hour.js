24f9ffbc5e917226cbf8623212e0305529b723f3
ehdrn3020 <ehdrnsla3020@naver.com>
2025-09-21 10:42:24 +0900

    예제 쿼리 변경 2

--- mongodb/examples/Aggregation/count_by_hour.js
+++ mongodb/examples/Aggregation/count_by_hour.js
@@ -1,14 +1,5 @@
 // 방별, 시간대별 메시지 건수
 db.chat_messages.aggregate([
-  {$project:{
-    roomId:1,
-    hour: {$dateTrunc:{date:"$ts", unit:"hour"}}
-  }},
-  {$group:{_id:{room:"$roomId", hour:"$hour"}, cnt:{$sum:1}}},
-  {$sort: {"_id.room":1, "_id.hour":1}}
-]).forEach(doc => printjson(doc))
-
-db.chat_messages.aggregate([
   {
     $group: {
       _id: {
