8b936555746026608def84c4b16456d7d98dc07c
ehdrn3020 <ehdrnsla3020@naver.com>
2025-09-21 10:31:03 +0900

    예제 쿼리 변경

--- mongodb/examples/Aggregation/count_by_hour.js
+++ mongodb/examples/Aggregation/count_by_hour.js
@@ -7,3 +7,17 @@ db.chat_messages.aggregate([
   {$group:{_id:{room:"$roomId", hour:"$hour"}, cnt:{$sum:1}}},
   {$sort: {"_id.room":1, "_id.hour":1}}
 ]).forEach(doc => printjson(doc))
+
+db.chat_messages.aggregate([
+  {
+    $group: {
+      _id: {
+        room: "$roomId",
+        hour: { $dateTrunc: { date: "$ts", unit: "hour" } }
+      },
+      cnt: { $sum: 1 }
+    }
+  },
+  { $sort: { "_id.room": 1, "_id.hour": 1 } },
+  { $project: { _id: 0, room: "$_id.room", hour: "$_id.hour", cnt: 1 } }
+]).forEach(doc => printjson(doc));
--- mongodb/examples/Aggregation/ranking_top_word.js
+++ mongodb/examples/Aggregation/ranking_top_word.js
@@ -1,20 +1,33 @@
 db.chat_messages.aggregate([
-  {$project:{
-    roomId:1,
-    hour: {$dateTrunc:{date:"$ts", unit:"hour"}},
-    words: {$split:[{$toString:"$message"}, " "]}
-  }},
-  {$unwind:"$words"},
-  {$group:{_id:{room:"$roomId", hour:"$hour", word:"$words"}, count:{$sum:1}}},
-  {$set:{
-    room:"$_id.room", hour:"$_id.hour", word:"$_id.word"
-  }},
-  {$setWindowFields:{
-    partitionBy:{room:"$room", hour:"$hour"},
-    sortBy:{count:-1},
-    output:{ rnk:{$rank:{} } }
-  }},
-  {$match:{ rnk: {$lte: 3} }},         // 상위 3개
-  {$project:{_id:0, room:1, hour:1, word:1, count:1, rnk:1}},
-  {$sort:{room:1, hour:1, rnk:1}}
-]).forEach(doc => printjson(doc))
+  {
+    $project: {
+      roomId: 1,
+      ts: 1,
+      words: { $split: [ { $toString: "$message" }, " " ] }
+    }
+  },
+  { $unwind: "$words" },
+  {
+    $group: {
+      _id: {
+        room: "$roomId",
+        hour: { $dateTrunc: { date: "$ts", unit: "hour" } },
+        word: "$words"
+      },
+      count: { $sum: 1 }
+    }
+  },
+  // window 파티션 키를 최상위로 올림(hoist)
+  { $set: { room: "$_id.room", hour: "$_id.hour", word: "$_id.word" } },
+  {
+    $setWindowFields: {
+      partitionBy: { room: "$room", hour: "$hour" },
+      sortBy: { count: -1 },
+      output: { rnk: { $rank: {} } }
+    }
+  },
+  { $match: { rnk: { $lte: 3 } } },
+  // 최종 출력용 프로젝션은 마지막
+  { $project: { _id: 0, room: 1, hour: 1, word: 1, count: 1, rnk: 1 } },
+  { $sort: { room: 1, hour: 1, rnk: 1 } }
+]).forEach(doc => printjson(doc));
